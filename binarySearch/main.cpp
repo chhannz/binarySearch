8117d3afd8f1e60e81ad0c9637074f06a3fa78af
Hanny Haryanto <77project@gmail.com>
2019-01-02 11:55:28 +0700

    rev1

--- binarySearch/main.cpp
+++ binarySearch/main.cpp
@@ -7,9 +7,42 @@
 //
 
 #include <iostream>
+#include <vector>
+
+using namespace std;
+
+long int binarySearch(vector <int> numb, unsigned long int low, unsigned long int high, int key);
+
+vector<int> number {2,5,8,12,16,23,38,56,72,91};
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    int numberToFound;
+    long int result;
+    unsigned long int index;
+    cout << "Input number to be found : ";
+    cin >> numberToFound;
+    index = binarySearch(number, 0, number.size()-1, numberToFound);
+    if (index == -1) cout << "not found !" << endl;
+        else {
+            result = number[index];
+            cout << "-->found : " << result << endl;
+            cout << "-->index : " << index << endl;
+        }
+    //system("pause");
     return 0;
+}
+
+long int binarySearch (vector<int> numb, unsigned long int low, unsigned long int high, int key) {
+    unsigned long int mid = (low + high) / 2;
+    while (low <= high) {
+        if (numb[mid] < key) {
+            low = mid + 1;
+            mid = (low + high) / 2;
+        } else if (numb[mid] > key) {
+            high = mid - 1;
+            mid = (low + high) / 2;
+        } else return mid;
+    }
+    
+    return -1;
 }
